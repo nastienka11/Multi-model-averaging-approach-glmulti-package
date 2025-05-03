07bc16e04de0c3e986d8b60266769253c42a2756
Nastienka Yael Perez Jimenez <bio.nastienka@gmail.com>
2025-05-03 01:09:53 -0600

    Update and rename 4_AnovaVar.Rmd to 4_AnovaVar.R

--- 4_AnovaVar.R
+++ 4_AnovaVar.R
@@ -2,55 +2,45 @@
 # Div_0
 
 ## Anova
-```{r}
 anova(mur_Div_0@objects[[2]], mur_Div_0@objects[[1]],  test="F")
-```
 
 ##Explained variance
-```{r}
 mur_Div_0_0 <- glm(Div_0_log ~ 1, data = land_Div_0_mur)
 summary(mur_Div_0_0)
 
 mur_Div_0_all <- glm(Div_0_log ~ Var1 + Var2 + Var3 + Var4, data = land_Div_0_mur)
 summary(mur_Div_0_all)
 
-(Residual deviance/Null deviance) * 100    
-```
+#### (Residual deviance/Null deviance) * 100    
+
 
 # Div_1
 
 ## Anova
-```{r}
 anova(mur_Div_1@objects[[2]], mur_Div_1@objects[[1]], test="F")
-```
 
 ##Explained variance
-```{r}
 mur_Div_1_0 <- glm(Div_1_log ~ 1, data = land_Div_1_mur)
 summary(mur_Div_1_0)
 
 mur_Div_1_all <- glm(Div_1_log ~ Var1 + Var2 + Var3 + Var4, data = land_Div_1_mur)
 summary(mur_Div_1_all)
 
-(Residual deviance/Null deviance) * 100    
-```
+#### (Residual deviance/Null deviance) * 100    
 
 
 # Div_2
 
 ## Anova
-```{r}
 anova(mur_Div_2@objects[[2]], mur_Div_2@objects[[1]], test="F")
-```
 
 ##Explained variance
-```{r}
 mur_Div_2_0 <- glm(Div_2_log ~ 1, data = land_Div_2_mur)
 summary(mur_Div_2_0)
 
 mur_Div_2_all <- glm(Div_2_log ~ For + Urb + Agri + Het, data = land_Div_2_mur)
 summary(mur_Div_2_all)
 
-(Residual deviance/Null deviance) * 100    
-```
+#### (Residual deviance/Null deviance) * 100    
+
 
